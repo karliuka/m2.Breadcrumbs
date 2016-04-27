43393e355afe5ffbc84c431872f3f38e8eb2d894
Vitalii <wit.kharkov@gmail.com>
2016-04-27 21:42:07 +0300

    upload

---
+++ app/code/Faonni/Breadcrumbs/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Faonni_Breadcrumbs',
+    __DIR__
+);
